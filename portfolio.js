1b85f92549523a58ba150a8bf18fcd4cea2d3fc0
jeremiahcanlas <jeremiahcanlas@gmail.com>
2020-05-24 19:07:18 -0400

    smooth scroll update

--- portfolio.js
+++ portfolio.js
@@ -1,17 +1,10 @@
 
-// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-//     anchor.addEventListener('click', function (e) {
-//         e.preventDefault();
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
 
-//         document.querySelector(this.getAttribute('href')).scrollIntoView({
-//             behavior: 'smooth'
-//         });
-//     });
-// });
-$(document).on('click', 'a[href^="#"]', function(event) {
-    event.preventDefault();
-  
-    $('html, body').animate({
-      scrollTop: $($.attr(this, 'href')).offset().top
-    }, 1000);
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
   });
+});
